c203eed285b38fd9e961da7bdea69ade8e8bc36b
adlwar137 <adlerwarmbier@gmail.com>
2023-04-13 22:31:39 -0400

    fix warnings

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use chip8::Chip8;
 const OFFSET: i32 = 0x200;
 
 fn main() {
-    let mut rom: Vec<u8> = vec![
+    let rom: Vec<u8> = vec![
         0x63, 0x05, //0x200 LD      V3,     5
         0xD1, 0x25, //0x202 DRW     V1,     V2,     5
         0xF3, 0x1E, //0x204 ADDI    V3
@@ -12,7 +12,7 @@ fn main() {
     ];
     let mut chippy = Chip8::new();
     chippy.load_rom(rom);
-    for i in OFFSET..OFFSET + 40 {
+    for _ in OFFSET..OFFSET + 40 {
         chippy.tick();
     }
 }
